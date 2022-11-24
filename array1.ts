f7540bb075c6205d05227e65de12f6ed0d517253
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 16:52:48 +0700

    Update 6.2

--- array1.ts
+++ array1.ts
@@ -4,3 +4,13 @@ names.push("Worawit");
 console.log(names[0]);
 console.log(names[1]);
 console.log(names.length);
+
+for(let i=0;i<names.length;i++) {
+    console.log(names[i]);
+}
+for(let ind in names) {
+    console.log(names[ind]);
+}
+names.forEach(function(names) {
+    console.log(names);
+});
