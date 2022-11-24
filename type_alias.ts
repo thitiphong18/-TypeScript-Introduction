3688e0a5b670d48eeab2b5534081e37725facca7
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 18:12:02 +0700

    Update 14.2

--- type_alias.ts
+++ type_alias.ts
@@ -12,6 +12,12 @@ const carYear: CarYear = 2001;
 const carType: CarType = "Toyota";
 const carModel: CarModel = "Corolla";
 
+const car1: Car = {
+    year: 2001,
+    type: "Nissan",
+    model: "XXX",
+}
+console.log(car1);
 
 
 
