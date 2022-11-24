ccab4f9c2d47f394266f593825ff7bd16dda8ea1
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 18:36:03 +0700

    Update 16.2

--- union_type1.ts
+++ union_type1.ts
@@ -1,6 +1,11 @@
 function printstatusCode(code: string|number) {
-    console.log(`My status code is ${code} ${typeof code} `)
+    if(typeof code === 'string') {
+        console.log(`My status code is ${code.toUpperCase()} ${typeof code} `)
+    } else {
+        console.log(`My status code is ${code} ${typeof code} `)
+    }
+    
 }
 
 printstatusCode(404);
-printstatusCode("404");
+printstatusCode("abc");
