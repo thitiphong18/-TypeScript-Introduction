9608e1c3b09498ff49594af6fd5126c414034469
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 19:25:15 +0700

    Update 19.2

--- class_example.ts
+++ class_example.ts
@@ -1,7 +1,12 @@
 class Person {
-    name: string;
+    public constructor(private readonly name:string){
+    }
+    public getName(): string {
+        return this.name;
+    } 
 }
 
-const person = new Person();
-person.name ="Jane";
+const person = new Person("Jane");
 console.log(person);
+console.log(person.getName());
+
