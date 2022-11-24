2e3894e6edf04b76b4f404e7cb6fad061547b132
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 18:19:12 +0700

    Update 15.2

--- interface1.ts
+++ interface1.ts
@@ -2,13 +2,20 @@ interface Rectangle {
     width: number;
     height: number;
 }
-
+interface ColoredRectangle extends Rectangle {
+    color: string 
+}
 const rectangle: Rectangle = {
     width: 20,
     height: 10,
 }
 console.log(rectangle);
-
+const coloredRectangle: ColoredRectangle = {
+    width: 20,
+    height: 10,
+    color: "red"
+}
+console.log(coloredRectangle);
 
 
 
