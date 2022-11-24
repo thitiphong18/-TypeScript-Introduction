4d3cb624882dd84bb8ce2124bb3158db99b34e63
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 19:34:14 +0700

    Update 19.3

--- class_example.ts
+++ class_example.ts
@@ -10,3 +10,17 @@ const person = new Person("Jane");
 console.log(person);
 console.log(person.getName());
 
+interface Shape {
+    getArea: () => number;
+}
+class Rectangle1 implements Shape {
+    public constructor(protected readonly width:number,protected readonly height:number) {}
+    public getArea():  number {
+        return this.width * this.height;
+    }
+}
+
+const rect:Rectangle1 = new Rectangle1(50, 10) ;
+console.log(rect);
+console.log(rect.getArea());
+
