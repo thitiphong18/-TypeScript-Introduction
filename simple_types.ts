ea5352beee53f7f80c6df3cd9867bdbf8d6f9c41
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-24 16:00:08 +0700

    Update 2

---
+++ simple_types.ts
@@ -0,0 +1,4 @@
+let firstName: string ="Dylan";
+//firstName = 33;
+console.log(firstName);
+console.log(typeof firstName);
---
+++ simple_types2.ts
@@ -0,0 +1,4 @@
+let  json:any = JSON.parse("55");
+console.log(typeof json);
+json ="55";
+console.log(typeof json);
